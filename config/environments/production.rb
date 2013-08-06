2de7b6bce6d5e25fb4a8445f211e98f739f3cc0f
bvlaar <bvlaar10@gmail.com>
2013-08-06 11:42:07 -0400

    Updated production config with sendgrid information

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,16 @@ Longbets::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['bvlaar'],
+  :password       => ENV['ovrclkr10'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
 
   # Enable threaded mode
   # config.threadsafe!
