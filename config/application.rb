03f4f855a91bef5205db959420acd80428966dba
bvlaar <bvlaar10@gmail.com>
2013-08-13 10:14:44 -0400

    Prepped configs for heroku deployment

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,9 @@ module Longbets
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
 
+    # To get devise working on Heroku deployment
+    config.assets.initialize_on_precompile = false
+
     # Enable the asset pipeline
     config.assets.enabled = true
     # config.assets.initialize_on_precompile = false
